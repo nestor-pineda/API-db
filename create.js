5f8c2ad9e0d3dbbf87731d8ed549888672ceaa4b
nestor-pineda <npineda@tbscg.com>
2024-04-08 11:21:09 +0200

    Filter by name

--- create.js
+++ create.js
@@ -1,9 +1,7 @@
-// create a querySelector to get the form in the create.html file
-// create.js
 const form = document.querySelector('form');
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
-  const formData = new FormData(form);
+  const formData = new FormData(form); // FormData: is a built-in browser API that allows you to easily construct a set of key/value pairs representing form fields and their values.
   const url = 'http://localhost:3000/players';
   const response = await fetch(url, {
     method: 'POST',
--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-const fetchPlayers = async () => {
-  const url = new URL('http://localhost:3000/players');
-  
-  // Set query parameters
-  const params = {
-    '_limit': '20',
-    '_sort': 'name',
-  };
-  
-  Object.entries(params).forEach(([key, value]) => {
-    url.searchParams.append(key, value);
-  });
-  const response = await fetch(url);
-  return await response.json();
-};
+const playersContainer = document.querySelector('.players');
 
-window.addEventListener('DOMContentLoaded', async () => {
-  const playersContainer = document.querySelector('.players');
-  const players = await fetchPlayers();
+const createPlayerElement = (container) => {
+  return (player) => {
+    // console.log(player);
+    const playerElement = document.createElement('div');
+    playerElement.classList.add('playerItem');
+
+    const { name, team, id } = player;
+
+    const nameElement = document.createElement('h2');
+    nameElement.textContent = name;
+
+    const teamElement = document.createElement('p');
+    teamElement.textContent = `Team: ${team}`;
+
+    const detailsLink = document.createElement('a');
+    detailsLink.textContent = 'View Details';
+    detailsLink.href = `player.html?id=${id}`;
+
+    playerElement.append(nameElement, teamElement, detailsLink);
+    console.log(playerElement);
+
+    container.append(playerElement);
+
+    console.log(container);
+  };
+}
+
+const fetchPlayers = async (name) => {
+  const url = new URL('http://localhost:3000/players');
+  const queryParams = { 'name': name || '' };
+  // const queryParams = { '_q': name || '' };
+
+  Object.entries(queryParams).forEach(([key, value]) => url.searchParams.append(key, value));
+
+  console.log(url);
+
+  const response = await fetch(url);
+  console.log(response);
+  const players = await response.json();
+  
   console.log(players);
 
+  playersContainer.innerHTML = '';
   players.forEach(createPlayerElement(playersContainer));
+};
 
-  function createPlayerElement(container) {
-    return (player) => {
-      const playerElement = document.createElement('div');
-      playerElement.classList.add('player');
 
-      const nameElement = document.createElement('h2');
-      nameElement.textContent = player.name;
+// Search players
+const searchPlayer = document.querySelector('.searchPlayer');
 
-      const teamElement = document.createElement('p');
-      teamElement.textContent = `Team: ${player.team}`;
-
-      const detailsLink = document.createElement('a');
-      detailsLink.textContent = 'View Details';
-      detailsLink.href = `player.html?id=${player.id}`;
-
-      playerElement.append(nameElement, teamElement, detailsLink);
-      container.appendChild(playerElement);
-    };
-  }
+searchPlayer.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const player = searchPlayer.name.value;// ".player" is the name atribute in the html form
+  fetchPlayers(player);
 });
+
+window.addEventListener('DOMContentLoaded', () => fetchPlayers());
